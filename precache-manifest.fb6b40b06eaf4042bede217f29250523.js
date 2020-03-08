7876eaf89efb3b486446c357095f90347878f2bd
Akash Jeganath <akashjoffl@icloud.com>
2020-03-09 00:35:32 +0530

    Updates

---
+++ precache-manifest.fb6b40b06eaf4042bede217f29250523.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecbbcca8af77439a8e11ff69359f5e5",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "2bb990b005e4cf2ed7bc",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "68f99c6cb3d6689b2665",
+    "url": "/Robofriends/static/css/main.5038c6a5.chunk.css"
+  },
+  {
+    "revision": "2bb990b005e4cf2ed7bc",
+    "url": "/Robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f99c6cb3d6689b2665",
+    "url": "/Robofriends/static/js/main.0a06b828.chunk.js"
+  },
+  {
+    "revision": "a538b29c6e4df7565c19",
+    "url": "/Robofriends/static/js/runtime-main.eef40706.js"
+  },
+  {
+    "revision": "600777957382413e114fbb43d380b879",
+    "url": "/Robofriends/static/media/Quantify Bold.60077795.woff"
+  },
+  {
+    "revision": "d9536625a650c0219638fb4fad777744",
+    "url": "/Robofriends/static/media/Quantify.d9536625.woff"
+  }
+]);
